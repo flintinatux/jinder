137172e7801b2919684ac85d7ad638d3d0be5d27
Scott McCormack <mail@madhackerdesigns.com>
2012-12-27 19:38:23 -0500

    New jinder package

---
+++ src/com/madhackerdesigns/jinder/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+ * Provides class necessary to connect to and communicate
+ * with a Campfire room. Implements the published Campfire
+ * API found at {@link https://github.com/37signals/campfire-api}.
+ * 
+ * @author scott
+ */
+package com.madhackerdesigns.jinder;
