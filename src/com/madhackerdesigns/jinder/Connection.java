4512b5071db86ae4b6cdd6cab4c7a205fb173470
Scott McCormack <mail@madhackerdesigns.com>
2012-12-29 22:53:56 -0500

    Added requestFactory builder.

--- src/com/madhackerdesigns/jinder/Connection.java
+++ src/com/madhackerdesigns/jinder/Connection.java
@@ -1,45 +1,82 @@
 package com.madhackerdesigns.jinder;
 
+import java.io.IOException;
 import java.net.URI;
 
+import com.google.api.client.http.ExponentialBackOffPolicy;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonObjectParser;
 
 public class Connection {
 
   private static final String HOST = "campfirenow.com";
   private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
   
-  private JsonFactory jsonFactory;
+  private static HttpRequestFactory connection;
+  
   private String subdomain;
   private URI uri;
   private ConnectionOptions options;
   private String token;
   
   public Connection(String subdomain) {
-    setupConnection(subdomain, new ConnectionOptions());
+    buildConnection(subdomain, new ConnectionOptions());
   }
 
   public Connection(String subdomain, ConnectionOptions options) {
-    setupConnection(subdomain, options);
+    buildConnection(subdomain, options);
   }
   
   public void setJsonFactory(JsonFactory jsonFactory) {
-    this.jsonFactory = jsonFactory;
   }
   
-  private void setupConnection(String subdomain, ConnectionOptions options) {
+  public boolean usingSSL() {
+	  return options.ssl;
+  }
+  
+  // private methods
+  
+  private HttpRequestFactory connection() {
+    if (connection == null) {
+      connection = HTTP_TRANSPORT.createRequestFactory(setConnectionOptions());
+    }
+    return connection;
+  }
+  
+  private void buildConnection(String subdomain, ConnectionOptions options) {
     this.subdomain = subdomain;
     this.options = options;
     this.uri = URI.create((options.ssl ? "https" : "http") + "://" + subdomain + "." + HOST);
     this.token = options.token;
+    
+    connection();
   }
+  
+  private HttpRequestInitializer setConnectionOptions() {
+    return new HttpRequestInitializer() {
+      
+      @Override
+      public void initialize(HttpRequest request) throws IOException {
+        request.setBackOffPolicy(new ExponentialBackOffPolicy());
+        request.setParser(new JsonObjectParser(options.jsonFactory));
+      }
+    };
+  }
+  
+  // embedded classes
 
   public class ConnectionOptions {
+    JsonFactory jsonFactory;
+    String proxy;
     boolean ssl = true;
     boolean sslVerify = true;
-    String proxy;
     String token;
+    String username;
+    String password;
   }
 }
